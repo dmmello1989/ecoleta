b21bbc93f2251f9338e7bf923c6d6b5756474db1
Daniel Mello <dmmello1989@gmail.com>
2020-06-12 18:49:47 -0300

    Image dropzone completed

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -38,6 +38,8 @@ const CreatePoint = () => {
     whatsapp: ''
   });
 
+  const [selectedFile, setSelectedFile] = useState<File>();
+
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
 
@@ -126,16 +128,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(', '));
+      
+      if(selectedFile) {
+        data.append('image', selectedFile)
+      }
 
     setSuccess({
       map: 'map-hidden',
@@ -161,8 +167,9 @@ const CreatePoint = () => {
       <form onSubmit={handleSubmit}>
         <h1>Register a<br/> collection point</h1>
 
-        <Dropzone />
-        {/* 33:30 */}
+        <Dropzone
+          onFileUploaded={setSelectedFile}
+        />
 
         <fieldset>
           <legend>
